d2a40c515f34749800c93ef02c839d7b7bf991f8
fayshal69 <foyshalbinamir@gmail.com>
2025-03-17 05:01:55 +0600

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,7 +18,7 @@ export const middleware = async (request: NextRequest) => {
     } else {
       return NextResponse.redirect(
         new URL(
-          `https://seconddeal-backend-production.up.railway.app/login?redirectPath=${pathname}`,
+          `https://second-deal-market.vercel.app/login?redirectPath=${pathname}`,
           request.url
         )
       );
